809329269bde2ece3574a39f23bb8f749d65ba87
Rakibul Islam <kdrakibbd@gmail.com>
2023-08-28 01:14:31 +0600

    Create ResponseAPI and complete user controller

--- src/main/java/com/rakib/blog/controller/UserController.java
+++ src/main/java/com/rakib/blog/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.rakib.blog.controller;
 
 import com.rakib.blog.entities.User;
+import com.rakib.blog.payloads.ApiResponse;
 import com.rakib.blog.payloads.UserDto;
 import com.rakib.blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,5 +22,31 @@ public class UserController {
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+
+    // update user
+    @PutMapping("/updateuser/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+        UserDto updatedUser = this.userService.updateUser(userDto, userId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // delete user
+    @DeleteMapping("/delete-user/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId) {
+        this.userService.deleteUser(userId);
+        return new ResponseEntity<>(new ApiResponse("User deleted Successfully", true), HttpStatus.OK);
+    }
+
+    // get all users
+    @GetMapping("/allusers")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    // get all users
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<UserDto> getAllUsers(@PathVariable Integer userId) {
+        return ResponseEntity.ok(this.userService.getUserById(userId));
     }
 }
